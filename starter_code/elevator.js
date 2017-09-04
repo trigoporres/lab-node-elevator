f6b0f97edf1208e8acb0659aa1f39a6ec7a3566b
trigoporres <santiagotrigoporres@gmail.com>
2017-09-04 23:36:48 +0200

    terminado

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,48 @@
+/* jshint esversion:6*/
+
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.waitingList = [];
+    this.destiny = [];
+    this.empty = false;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.acction = setInterval(() => { this.updateEnter();}, 1000);
+  }
+  stop(){
+    clearInterval(this.acction);
+  }
+  updateEnter() {
+    (this.floor < this.requests[0]) ? this.floorUp() : (this.floor > this.requests[0]) ? this.floorDown() : (this.floor == this.requests[0]) ? this._passengersEnter() : false;
+    (this.floor < this.destiny[0] && this.empty == true) ? this.floorUp() : (this.floor > this.destiny[0] && this.empty == true) ? this.floorDown() : (this.floor == this.destiny[0] && this.empty == true) ? this._passengersLeave(): false;
+  }
+  _passengersEnter() {
+    this.empty = true;
+    this.requests.shift();
+    console.log(this.waitingList[0].name + " entra en el ascensor en planta " + this.floor );
+  }
+  _passengersLeave() {
+    this.destiny.shift();
+    this.stop();
+    console.log(this.waitingList[0].name + " sale del ascensor en planta " + this.floor);
+  }
+  floorUp() {
+    return (this.floor < this.MAXFLOOR) ? this.floor += 1 : false;
+  }
+  floorDown() {
+    return (this.floor > 0) ? this.floor -= 1 : false;
+  }
+  call(person) {
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+    this.destiny.push(person.destinationFloor);
+  }
+  log() {
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,10 @@
+/* jshint esversion:6*/
+
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator = new Elevator();
+const person = new Person("santi", 4, 0);
+elevator.call(person);
+
+elevator.start();
